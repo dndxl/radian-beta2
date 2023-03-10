5ac6bb2a0af9e187af47f870871191b46e3a3eef
dinda <dindaaullia@gmail.com>
2023-03-10 20:48:31 +0700

    update responsive

--- dist/JS/smp.js
+++ dist/JS/smp.js
@@ -20,7 +20,7 @@ window.onscroll = function() {
 };
 
 
-//caraousel alumni
+//caraousel testimoni 
 let slidercarousel = document.getElementById('slidercarousel');
 let slids = document.getElementById('slids');
 let card = slids.getElementsByTagName('li');
@@ -55,37 +55,37 @@ function after() {
         slids.style.marginLeft = ((+slids.style.marginLeft.slice(0, -3)) + cardWidth)+'px'
 }
 
-//caraousel testimoni
-let sliderContainer = document.getElementById('sliderContainer');
-let slider = document.getElementById('slider');
-let cards = slids.getElementsByTagName('li');
+// //caraousel testimoni
+// let sliderContainer = document.getElementById('sliderContainer');
+// let slider = document.getElementById('slider');
+// let cards = slids.getElementsByTagName('li');
 
-let elemetToShow = 2;
-if(document.body.clientWidth<1000){
-    elemetToShow = 1;
-}else if(document.body.clientWidth<1500){
-    elemetToShow = 3;
-}
+// let elemetToShow = 2;
+// if(document.body.clientWidth<1000){
+//     elemetToShow = 1;
+// }else if(document.body.clientWidth<1500){
+//     elemetToShow = 3;
+// }
 
-let sliderContainerWidth = sliderContainer.clientWidth;
+// let sliderContainerWidth = sliderContainer.clientWidth;
 
-let cardsWidth = sliderContainerWidth/elemetToShow;
+// let cardsWidth = sliderContainerWidth/elemetToShow;
 
-slider.style.width = cards.length*cardsWidth+'px';
-slider.style.transition='margin';
-slider.style.transitionDuration='1s';
+// slider.style.width = cards.length*cardsWidth+'px';
+// slider.style.transition='margin';
+// slider.style.transitionDuration='1s';
 
-for (let index = 0; index < cards.length; index++) {
-    const element = cards[index];
-    element.style.width = cardsWidth+'px';
-}
+// for (let index = 0; index < cards.length; index++) {
+//     const element = cards[index];
+//     element.style.width = cardsWidth+'px';
+// }
 
-function prev(){
-    if(+slider.style.marginLeft.slice(0, -3) != -cardsWidth * (cards.length - elemetToShow))
-        slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -3)) - cardsWidth)+'px'
-}
+// function prev(){
+//     if(+slider.style.marginLeft.slice(0, -3) != -cardsWidth * (cards.length - elemetToShow))
+//         slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -3)) - cardsWidth)+'px'
+// }
 
-function next() {
-    if (+slider.style.marginLeft.slice(0, -3) != 0)
-        slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -3)) + cardsWidth)+'px'
-}
+// function next() {
+//     if (+slider.style.marginLeft.slice(0, -3) != 0)
+//         slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -3)) + cardsWidth)+'px'
+// }
